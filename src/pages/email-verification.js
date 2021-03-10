2456be15e38ac7961d3e8ba2bea90f05ec2ccbb2
AG Liber <aliber@usc.edu>
2021-03-10 12:08:04 -0800

    feat: add email-verification link for when apple universal link fails

--- src/pages/email-verification.js
+++ src/pages/email-verification.js
@@ -2,7 +2,7 @@ import React from "react";
 import clsx from "clsx";
 import Layout from "@theme/Layout";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
-import useBaseUrl from "@docusaurus/useBaseUrl";
+import BrowserOnly from "@docusaurus/BrowserOnly";
 import styles from "./styles.module.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Container, Button, Alert } from "react-bootstrap";
@@ -18,16 +18,41 @@ function Support() {
             <h2>Welcome to the Banqdrop website! </h2>
             <br />
             <Alert variant="primary">
-              <h1>
+              <h3>
                 Please open the link on your device where Banqdrop is installed
-              </h1>
-              <p>It will you direct you into the app</p>
+              </h3>
+              <h4>It should direct you into the app</h4>
+              <hr />
+              <p>
+                You probably got here by opening your email verification link on
+                your computer (or other device than where the Banqdrop app is
+                installed)
+              </p>
+              <p></p>
             </Alert>
-            <h6>
-              You probably got here by opening your email verification link on
-              your computer
-            </h6>
-            <p>(or other device than where the Banqdrop app is installed)</p>
+            <h1>OR</h1>
+            <BrowserOnly>
+              {() => (
+                <Alert variant="primary">
+                  <h3>
+                    If you are already on your device where Banqdrop is
+                    installed
+                  </h3>
+                  <p>
+                    Please click this link:{" "}
+                    <a
+                      href={window.location.href.replace(
+                        "https://banqdrop.com",
+                        "banqdrop://"
+                      )}
+                      style={{ color: "blue" }}
+                    >
+                      Sign into Banqdrop
+                    </a>
+                  </p>
+                </Alert>
+              )}
+            </BrowserOnly>
           </Container>
         </header>
       </main>
